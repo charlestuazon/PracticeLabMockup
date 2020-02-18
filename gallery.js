c89d31a48d2c6b51464fd40b6cebb52a60a9f7bc
unknown <charleschristian.tuazon.iics@ust.edu.ph>
2020-02-18 23:47:23 +0800

    first commit

---
+++ gallery.js
@@ -0,0 +1,12 @@
+if (document.getElementById('login')){
+    alert("There is no login details");
+}
+else if (document.getElementById('password')){
+    alert("Please type your password");
+}
+else if (document.getElementById('cpassword')){
+    alert("Please type again the same password");
+}
+else (document.getElementById('email')){
+    alert("Please type your email");
+}
